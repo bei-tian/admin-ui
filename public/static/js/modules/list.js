0fc494084b95e52493de424ed362cd55fc6f2275
linxiaoji <lxj8749@qq.com>
2019-06-05 16:05:18 +0800

    后台分页问题处理

--- public/static/js/modules/list.js
+++ public/static/js/modules/list.js
@@ -4,6 +4,16 @@ layui.define(['layer', 'element', 'laypage'], function (exports) {
     layui.laypage.render({
         elem: 'page'
         ,count: $page.attr('count')
+        ,curr: $page.attr('curr')
+        ,jump: function(obj, first) {
+            //首次不执行
+            if (!first) {
+                var arr = location.href.split('page');
+                var newUrl = arr[0] + '?page=' + obj.curr;
+                newUrl = newUrl.replace('??', '?').replace('&?', '&');
+                location.href = newUrl;
+            }
+        }
     });
 
 
