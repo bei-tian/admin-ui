96ddc93f24f7ec227c49ba030e9ada691189d5fa
linxiaoji <lxj8749@qq.com>
2019-07-08 16:01:27 +0800

    增加模板页面

---
+++ application/admin/views/tpl/Tpl.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Tpl extends Curd {
+	public function __construct() {
+		parent::__construct();
+
+		$this->conf['index'] = [
+            'search'=> ['title-like'],
+		];
+	}
+}
