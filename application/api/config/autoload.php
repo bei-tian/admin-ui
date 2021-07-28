2f44e9c7041d683ec0b17d8a790c91d46a19482b
linxiaoji <lxj8749@qq.com>
2021-07-28 14:15:44 +0800

    删除一些没用代码

--- application/api/config/autoload.php
+++ application/api/config/autoload.php
@@ -39,7 +39,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |  $autoload['packages'] = array(APPPATH.'third_party', '/usr/local/shared');
 |
 */
-$autoload['packages'] = array(COPATH);
 
 /*
 | -------------------------------------------------------------------
